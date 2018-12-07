90a379523151647a6348cf6b7aff0d21f6a6273a
Serban <deaconu.serban@gmail.com>
2018-12-07 14:10:33 +0200

    Updates

---
+++ precache-manifest.e11262de1f4e57d3f375f53c4d2ae673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79d212e0b17619207a8",
+    "url": "/calculatorjs/static/js/runtime~main.b79d212e.js"
+  },
+  {
+    "revision": "250030b550f7dc0ebc5e",
+    "url": "/calculatorjs/static/js/main.250030b5.chunk.js"
+  },
+  {
+    "revision": "0eec96f4ccba5611a9c2",
+    "url": "/calculatorjs/static/js/1.0eec96f4.chunk.js"
+  },
+  {
+    "revision": "250030b550f7dc0ebc5e",
+    "url": "/calculatorjs/static/css/main.c62df0c7.chunk.css"
+  },
+  {
+    "revision": "00a53a950340924feddd4ce24104b3e5",
+    "url": "/calculatorjs/index.html"
+  }
+];
